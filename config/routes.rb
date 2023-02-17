f798b61eb16e2856844615116df55296d55e6018
Nandosts <fernando.jsts@gmail.com>
2023-02-17 19:37:19 -0300

    Organiza a navbar e os defaults

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "chat#index"
 
   get '/chat', to: 'chat#index'
   get '/image_variants', to: 'image_variants#index'
