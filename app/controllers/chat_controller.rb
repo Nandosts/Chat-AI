1310c633ef52c50bb361d7aee10317a6f573a36a
Nandosts <fernando.jsts@gmail.com>
2023-02-18 10:44:29 -0300

    Mexendo mais um pouco no chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -18,9 +18,9 @@ class ChatController < ApplicationController
     # begin
     response = client.completions(
       parameters: {
-        model: ENV['OPENAI_MODEL'] || 'text-davinci-002',
+        model: ENV['OPENAI_MODEL'] || 'text-davinci-003',
         prompt: message,
-        max_tokens: ENV['OPENAI_MAX_TOKENS'] || 150,
+        max_tokens: ENV['OPENAI_MAX_TOKENS'] || 1000,
         n: ENV['OPENAI_N'] || 1,
         stop: ENV['OPENAI_STOP'] || nil,
         temperature: ENV['OPENAI_TEMPERATURE'] || 0.7
@@ -30,7 +30,7 @@ class ChatController < ApplicationController
     #   Rails.logger.error("OpenAI API Error: #{e}")
     #   return "Desculpe, ocorreu um erro ao gerar uma resposta."
     # end
-
+    puts(response)
     return response.dig('choices', 0, 'text') if response.dig('choices', 0, 'text').present?
 
     'Desculpe, não consegui gerar uma resposta a partir da entrada fornecida.'
