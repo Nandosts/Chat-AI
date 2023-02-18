2007946c7ce81dc537263927a05ec65d3d9f9154
Nandosts <fernando.jsts@gmail.com>
2023-02-18 18:15:44 -0300

    Adiciona configuração para corrigir webpack

--- config/application.rb
+++ config/application.rb
@@ -21,4 +21,5 @@ module ChatAi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+  ENV['NODE_OPTIONS'] = '--openssl-legacy-provider'
 end
